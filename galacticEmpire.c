eaa9c054cff5f3dc1689cdc51d337c1e2a20b8fe
Sebastian Klukas <sebastian.klukas@tum.de>
2021-12-18 23:51:06 +0100

    first try

--- galacticEmpire.c
+++ galacticEmpire.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cc65.h>
 #include <conio.h>
 #include <stdbool.h>
+#include <tgi.h>
+
+
+#ifndef DYN_DRV
+    #  define DYN_DRV       0
+#endif
+
+/* Driver stuff */
+static unsigned MaxX;
+static unsigned MaxY;
+static unsigned AspectRatio;
+
+
 
 // Global key variable
 int numPlayers = 1;
@@ -16,9 +31,77 @@ int **worlds;
 int key;
 char x,y=10;
 
+// todo could be made variable later
+int mapXLength = 20;
+int mapYLength = 20;
+
+int i;
+
+
+
+static void CheckError (const char* S)
+{
+    unsigned char Error = tgi_geterror ();
+
+    if (Error != TGI_ERR_OK) {
+        printf ("%s: %u\n", S, Error);
+        if (doesclrscrafterexit ()) {
+            cgetc ();
+       }
+        exit (EXIT_FAILURE);
+    }
+}
+
+ 
+
+#if DYN_DRV
+static void DoWarning (void)
+/* Warn the user that the dynamic TGI driver is needed for this program */
+{
+    printf ("Warning: This program needs the TGI\n"
+            "driver on disk! Press 'y' if you have\n"
+            "it - any other key exits.\n");
+    if (tolower (cgetc ()) != 'y') {
+        exit (EXIT_SUCCESS);
+    }
+    printf ("OK. Please wait patiently...\n");
+}
+#endif
+
+
+
+
+
 // functions
 void drawMap()
-{}
+{
+    tgi_clear ();
+    tgi_line(0,0,10,10);
+
+
+    /*
+    chlinexy(0, 0, 10);
+    cputsxy(10, 0, "A");
+    chlinexy(12, 0, 10);
+    chlinexy(0, 1, 10);
+
+
+
+    for (i = 0; i < mapXLength; ++i)
+    {
+        cvlinexy(i, 0, mapYLength);    
+    }
+    chlinexy(0, 0, 1);
+
+    cputsxy(30, 5, "+++");
+    */
+
+    //for (i = 0; i < mapYLength; ++i)
+    //{
+    //    chlinexy(0, i, mapXLength);    
+    // }
+    cgetc();
+}
 
 void drawTable()
 {}
@@ -30,8 +113,29 @@ void updateInput()
 
 int main()
 {
-    
-    
+#if DYN_DRV
+    /* Warn the user that the tgi driver is needed */
+    DoWarning ();
+
+    /* Load and initialize the driver */
+   tgi_load_driver (tgi_stddrv);
+    CheckError ("tgi_load_driver");
+#else
+    /* Install the driver */
+    tgi_install (tgi_static_stddrv);
+    CheckError ("tgi_install");
+#endif
+
+    tgi_init ();
+    CheckError ("tgi_init");
+
+    /* Get stuff from the driver */
+    MaxX = tgi_getmaxx ();
+    MaxY = tgi_getmaxy ();
+    AspectRatio = tgi_getaspectratio ();
+
+
+    /// todo: Graphics stuff goes here 
     /* Clear Screen */
     clrscr();
  
@@ -42,42 +146,23 @@ int main()
     drawTable();
 
 
+#if DYN_DRV
+    /* Unload the driver */
+    tgi_unload ();
+#else
+    /* Uninstall the driver */
+    tgi_uninstall ();
+#endif
 
-    /* Loop until Q is pressed */
-    while ((key  = cgetc()) != 'Q')
-    {
-        
-        /*
-        // Delete the character
-        cputsxy(x,y,".");
+    /* Reset the border */
+    // todo do later
+    // (void) bordercolor (Border);
  
-        // keys;
-        switch (key) 
-        { 
-            case 'w': 
-                y--; 
-                break; 
-            case 'a': 
-                x--; 
-                break; 
-            case 's': 
-                y++; 
-                break; 
-            case 'd': 
-                x++; 
-                break; 
- 
-            default:
-                break;
-        }
- 
-        cputsxy(x,y,"@");
-        */
-    }
-    
-    
-    cursor(1);
-    
-    return(0);
+    /* Done */
+
+    while(1)
+    {}
+
+    printf ("Done\n");
+    return EXIT_SUCCESS;
 }
-
