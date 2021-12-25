2426a5ff0e68636cd21a24571e18e0598b4d2327
Oliver Klukas <oliverklukas@t-online.de>
2021-12-25 15:43:21 +0100

    scraped tgi

--- GalacticEmpire/src/main.c
+++ GalacticEmpire/src/main.c
@@ -2,19 +2,7 @@
 #include <stdlib.h>
 #include <cc65.h>
 #include <conio.h>
-#include <tgi.h>
 #include "graphics.h"
-
-/*****************************************************************************/
-/*                                 Macros                                    */
-/*****************************************************************************/
-
-
-#ifndef DYN_DRV
-#  define DYN_DRV       0
-#endif
-
-#define COLOR_BORD      COLOR_GREEN
 
 
 /*****************************************************************************/
@@ -53,44 +41,6 @@ int availableLocations[400];
 /*                              Functions                                    */
 /*****************************************************************************/
 
-
-/**
- * Custom function to print out tgi errors.
- *
- * <p>Source: https://github.com/cc65/cc65/blob/master/samples/tgidemo.c
- *
- * @param S - Function name that throws the error given as String.
- */
-static void CheckError(const char *S) {
-    unsigned char Error = tgi_geterror();
-
-    if (Error != TGI_ERR_OK) {
-        printf("%s: %u\n", S, Error);
-        if (doesclrscrafterexit()) {
-            cgetc();
-        }
-        exit(EXIT_FAILURE);
-    }
-}
-
-
-#if DYN_DRV
-/**
- * Warns the user that the dynamic TGI driver is needed for this program.
- *
- * <p>Source: https://github.com/cc65/cc65/blob/master/samples/tgidemo.c
- */
-static void DoWarning ()
-{
-    printf ("Warning: This program needs the TGI\n"
-            "driver on disk! Press 'y' if you have\n"
-            "it - any other key exits.\n");
-    if (tolower (cgetc ()) != 'y') {
-        exit (EXIT_SUCCESS);
-    }
-    printf ("OK. Please wait patiently...\n");
-}
-#endif
 
 /**
  * Checks if a planet location is not already taken and occupies it if possible.
@@ -231,7 +181,7 @@ void generateGalaxy() {
 /**
  * Initialises the game questions.
  */
-void initGameInputs() {
+void initGameInputs() { // TODO: new conio in graphics
     // Loop variables.
     int i;
 
@@ -292,13 +242,11 @@ void game() {
 
     // Plot start screen.
     //startScreen(); //TODO
-    tgi_clear();
 
     // Handle initial questions.
     initGameInputsMock(); //TODO: switch back to questions initGameInputs();
-    tgi_clear();
 
-    // Updates indices to update whole map
+    // Updates indices to update whole map.
     for(i = 0; i < 40; i++){
         if(i < numWorlds){
             indices[i] = 1;
@@ -308,7 +256,7 @@ void game() {
     }
 
     // Initialize everything that shouldn't be changed on the map.
-    initGameGraphics();
+    initGameGraphics(); // TODO: new in graphics
 
     // Initialize world and map based on player acceptance.
     do {
@@ -349,41 +297,14 @@ int main() {
     // Save the previous border color to reset later on.
     unsigned char Border;
 
-#if DYN_DRV
-    // Warn the user that the tgi driver is needed.
-    DoWarning ();
-    // Load and initialize the driver.
-    tgi_load_driver (tgi_stddrv);
-    CheckError ("tgi_load_driver");
-#else
-    // Install the driver.
-    tgi_install(tgi_static_stddrv);
-    CheckError("tgi_install");
-#endif
-
-    // Initialize the loaded graphics driver.
-    tgi_init();
-    CheckError("tgi_init");
-
     // Set the border colors.
     Border = bordercolor(COLOR_BORD);
-
-    // Clear the initial tgi drawing board.
-    tgi_clear();
 
     // Call main game function.
     game();
 
-#if DYN_DRV
-    // Unload the driver.
-    tgi_unload ();
-#else
-    // Uninstall the driver.
-    tgi_uninstall();
-#endif
-
     // Reset the border.
-    (void) bordercolor(Border);
+    bordercolor(Border);
 
     printf("Done\n");
     return EXIT_SUCCESS;
